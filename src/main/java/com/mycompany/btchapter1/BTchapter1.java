7127240e7895500a6b7d3f9d78f67a377efe0aef
Tudzlaanhhahaahha <nguyenthuhuong270680@gmail.com>
2026-01-14 11:50:45 +0700

    Nop bai Java chapter 1

---
+++ src/main/java/com/mycompany/btchapter1/BTchapter1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.btchapter1;
+
+/**
+ *
+ * @author laptop dell
+ */
+public class BTchapter1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
