b9a97ccef700de7f86a9f1adb98e383b7c831e20
Tudzlaanhhahaahha <nguyenthuhuong270680@gmail.com>
2026-01-26 16:18:48 +0700

    Submit BTchapter2

---
+++ src/main/java/com/mycompany/btchapterhai/BTchapterhai.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.btchapterhai;
+
+/**
+ *
+ * @author laptop dell
+ */
+public class BTchapterhai {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
